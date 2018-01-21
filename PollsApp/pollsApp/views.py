4792d9934d177330d810293196cd49619a6a57c2
gsagar4145 <sagargaurav786@gmail.com>
2018-01-22 02:10:40 +0530

    Final commit

--- PollsApp/pollsApp/views.py
+++ PollsApp/pollsApp/views.py
@@ -1,25 +1,40 @@
-from django.shortcuts import render,get_object_or_404
-
-# Create your views here.
-from django.http import HttpResponse,HttpResponseRedirect
-from django.template import loader
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question,Choice
+from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'pollsApp/index.html', context)
 
-def detail(request, question_id):
-	question = get_object_or_404(Question,pk=question_id)
-	return render(request,'pollsApp/detail.html',{'question':question})
+class IndexView(generic.ListView):
+    template_name = 'pollsApp/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'pollsApp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pollsApp/results.html'
 def vote(request, question_id):
 	question = get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError,Choice.DoesNotExist):
+		return render(request,'pollsApp/detail.html',{
+			'question':question,
+			'error_message': "You didn't selected any choice",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+
+		return HttpResponseRedirect(reverse('pollsApp:results',args=(question_id,)))
+
